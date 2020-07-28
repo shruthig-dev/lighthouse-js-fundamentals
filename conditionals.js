e54d115f673e70d249b3a75a0eda6585f1e4ffe2
shruthig <shruthigangadharblr@gmail.com>
2020-07-28 23:16:57 +0000

    code snippet added

--- conditionals.js
+++ conditionals.js
@@ -14,7 +14,11 @@ console.log("Now you're ready to go outside!");
 
 const temperature = -1;
 
-if (temperature < 0) {
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+else if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
