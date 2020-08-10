4b872891b9e5609464c6823e2e4fea60c0be7ab9
shruthig <shruthigangadharblr@email.com>
2020-08-09 19:54:39 -0700

    added missing files

---
+++ hello.js
@@ -0,0 +1,11 @@
+const sayHello  = function (name) {
+    return "Hello, " + name;
+  }
+
+
+//sayHello("Caliban");
+//sayHello("Miranda");
+//sayHello("Ferdinand");
+
+const greeting = sayHello('John');
+console.log(greeting);
